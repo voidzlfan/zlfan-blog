11910f6a1e3e67d856061b352972f0d010d9ee45
voidzlfan <977933816@qq.com>
2021-03-08 20:59:31 +0800

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Button } from "antd"
+
+class App extends Component {
+
+  render() {
+    return (
+      <Button type="primary">你好</Button>
+    );
+  }
+}
+
+export default App;
